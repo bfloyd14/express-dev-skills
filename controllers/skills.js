e8680ceea22179b280fec16105a9e1bfc25a8be0
bfloyd14 <brendanflo1425@gmail.com>
2024-04-12 22:52:15 -0400

    add update and edit routes, controllers and views with checkbox

--- controllers/skills.js
+++ controllers/skills.js
@@ -56,10 +56,43 @@ function deleteSkill(req, res){
   })
 }
 
+function update(req, res){
+  //handle checkboc logic
+  req.body.have = !!req.body.have
+  console.log(req.body)
+  //find the skill by id and update
+  Skill.findByIdAndUpdate(req,params.skillId, req.body, {new:true})
+  .then(skill =>{
+    //redirect back to show view
+    res.redirect(`/skills/${req.params.skillId}`)
+  })
+  .catch(error =>{
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
+function edit(req, res){
+  //find the skill and pass it to render
+  Skill.findById(req.params.skillId)
+  .then(skill =>{
+    //render a view with a form (edit.ejs)
+    res.render('skills/edit',{
+      skill: skill
+    })
+  })
+  .catch(error =>{
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 export {
   index,
   newSkill as new,
   create,
   show,
   deleteSkill as delete,
+  update,
+  edit
 }
--- routes/skills.js
+++ routes/skills.js
@@ -6,12 +6,22 @@ const router = Router()
 // GET localhost:3000/users
 router.get('/', skillsCtrl.index )
 
+// Get localhost:3000/new
 router.get('/new', skillsCtrl.new)
 
+// Get localhost:3000/skills/:skillId
 router.get('/:skillId', skillsCtrl.show)
 
+// Get localhost:3000/skills/:skillId/edit
+router.get('/:skillId/edit', skillsCtrl.edit)
+
+// PUT localhost:3000/skills/:skillId
+router.put('/:skillId', skillsCtrl.update)
+
+// POST localhost:3000/skills
 router.post('/', skillsCtrl.create)
 
+// DELETE localhost:3000/skills/:skillId
 router.delete('/:skillId', skillsCtrl.delete)
 
 
